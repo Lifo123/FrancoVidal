092e1e4b199339cf23b49e89b4132357ec3f3262
Lifo123 <alberto_p-@hotmail.com>
2025-06-11 14:45:58 -0500

    Updates

---
+++ assets/Interface.Store.DyO9FG3J.js
@@ -0,0 +1,1 @@
+import{f as i}from"./ToastAssets.DyaLsPIA.js";i({asideRight:{isVisible:!1,isAnim:!1},asideLeft:{isVisible:!1,isAnim:!1}});
--- assets/User.Store.Dn9tK8Xi.js
+++
@@ -1,1 +0,0 @@
-import{m as o}from"./index.DWXiAXll.js";const s=o({user:"",token:""});export{s as $};
